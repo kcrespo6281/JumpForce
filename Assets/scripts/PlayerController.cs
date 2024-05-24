5800d7ee38fcd1182183441e886b6ad384d7f736
katelyn <crespok@roverkids.org>
2024-05-24 14:18:30 -0400

    almost done with 3.2

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +29,22 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision) 
     {
-        isOnGround = true;
+
+
+        if (collision.gameObject.CompareTag("Ground"))
+        { 
+            isOnGround = true;
+        }else if (collision.gameObject.CompareTag("Obstacle"))
+
+
+
+
+
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+
+        }
     }
 
 
