f5d9bd61c398dcec3329bb43ee08d12939806343
katelyn <crespok@roverkids.org>
2024-05-28 10:37:25 -0400

    finished

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+  
     // Start is called before the first frame update
     void Start()
     {
